bb7a5453cbbaee7a660840913af2e819185a4826
Nishad Sharma <nishad.sharma@hotmail.com>
2026-02-08 18:31:36 +1100

    add CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,14 @@ async function bootstrap() {
 
     // helmet sets secure http response headers. helps with clickjacking, xss attacks, prevents mime sniffing.
     app.use(helmet());
+
+    // restricts cross-domain requests. we allow frontend to access api 
+    app.enableCors({
+        origin: 'http://localhost:4200',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true, // allow cookies/auth headers -req for jwt auth
+    });
+
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
